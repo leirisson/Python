af5ee466daad4df2292fecc9e19a312d5e4e9511
leirisson <leirissonsouza99@gmail.com>
2024-08-14 13:02:32 -0400

    subir
    subir

---
+++ 6.sql-Alchemy/0.exercicios/4.Sistema_de_Vendas_Online/configs/configs.py
@@ -0,0 +1,17 @@
+from pydantic_settings import BaseSettings
+from sqlalchemy.ext.declarative import declarative_base
+from typing import ClassVar
+
+class Settings(BaseSettings):
+    # configurações gerais da APLICAÇÂO
+    API_V1_STR: str = '/api/v1'
+    DB_URL: str = "mysql+aiomysql://root:123456@localhost:3307/vendas"
+    DB_BASE_MODEL: ClassVar = declarative_base()
+
+
+    class config:
+        case_sensitive = True
+
+settings = Settings()
+
+
