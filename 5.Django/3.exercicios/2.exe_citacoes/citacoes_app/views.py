9bece1c911ed0caf804f53321a6739c9acd6c0a8
leirisson <leirissonsouza99@gmail.com>
2024-09-10 17:36:01 -0400

    subir

--- 5.Django/3.exercicios/2.exe_citacoes/citacoes_app/views.py
+++ 5.Django/3.exercicios/2.exe_citacoes/citacoes_app/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def pagina_incial(request):
-    return render(request, "citacoes/pages/index.html")
+    return render(request, "app_cadastro/pages/home.html")
