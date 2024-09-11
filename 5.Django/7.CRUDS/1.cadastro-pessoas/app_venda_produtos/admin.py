95a82b0617bda6dc83c8690de432f7d5d4c391f7
leirisson <leirissonsouza99@gmail.com>
2024-09-11 17:02:35 -0400

    subir

---
+++ 5.Django/7.CRUDS/1.cadastro-pessoas/app_venda_produtos/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+import models 
+
+# Register your models here.
+
+admin.site.register(models.Cadastro_cliente)
+admin.site.register(models.Cadastro_de_produto)
+admin.site.register(models.Cadastro_marca)
+admin.site.register(models.Vendas)
